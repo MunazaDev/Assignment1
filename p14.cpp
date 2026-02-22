b9fab94b1507b95a9314915aeec1fb5d03072922
Munaza Rauf <munazarauf31@gmail.com>
2026-02-22 09:02:00 -0800

    Add files via upload

---
+++ p14.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+main () {
+cout<<"             ------------------------------------            "<<endl<<endl;
+cout<<"                    o    ^____^                              "<<endl<<endl;
+cout<<"                      o   (oo)\\___________                  "<<endl<<endl;
+cout<<"                          (__)\\          )\\/\\             "<<endl<<endl;
+cout<<"                              ||--------w |                  "<<endl<<endl;
+cout<<"                              ||         ||                  "<<endl;
+
+
+
+}                            
