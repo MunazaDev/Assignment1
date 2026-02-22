d715eb3f49f67ac8e65b571c7dccd019c88c5e9d
Munaza Rauf <munazarauf31@gmail.com>
2026-02-22 09:14:43 -0800

    Add files via upload

---
+++ p21.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std ;
+main (){
+cout <<"              . . _ _ _ . . "<<endl;
+cout<<"            .  . .       . . . . "<<endl;
+
+cout<<"       .- - - - - - - - - - - - -."<<endl;
+
+cout<< "  .- - - - - - - - - - - - - - - -. "<<endl;
+
+
+cout<<"   - - - - - - - - - - - - - - -  . . "<< endl;
+cout <<"                                 . "<<endl;
+
+cout<<". - - - - - - - - - - - . .  "<<endl;
+cout<<".                       . . . "<< endl;
+
+cout<<"- - - - - - - - - - - . "<<endl;
+cout<<"                      . . "<<endl;
+
+cout<<". - - - - - - - - - - - - -. "<<endl;
+cout<<".                          . . . "<<endl;
+
+
+cout<<"   . - - - - - - - - - - - - - - - . "<<endl;
+cout<<"   .                               . . "<<endl;
+
+cout<<"     .  - - - - - - - - - -  - - - . "<<endl;
+cout<<"                                   . "<<endl;
+
+cout<<"          . - - - - - - - -. "<<endl;
+cout <<"       . .                . . "<<endl;
+
+
+}
+
