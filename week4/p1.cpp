4f43dfe84a0c87bab275f47f0668e897fe9338ec
Munaza Rauf <munazarauf31@gmail.com>
2026-03-08 11:25:43 -0700

    Add files via upload

---
+++ week4/p1.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+main () {
+string university;
+cout<<" are you going to university?";
+cin>> university;
+if (university=="yes" ){
+cout<<"I am going to university";
+}
+if (university=="no") { 
+cout<<" I am not going to university
